bf5b09c3e5844477fae180e567efe5bf2e2f09d1
Deivida Gaizauskaite <57774682+DEIVIDA@users.noreply.github.com>
2022-05-17 20:47:14 +0300

    6-10 Tasks

--- Task 7/script.js
+++ Task 7/script.js
@@ -4,9 +4,19 @@ Turimas "audi" objektas.
 Parašykite funkciją "showObjectKeys", kuri kaip argumentą priims objektą 
 ir grąžins visus jo "key" masyve.
 -------------------------------------------------------------------------- */
+
 const audi = {
-  make: 'audi',
-  model: 'A6',
+  make: "audi",
+  model: "A6",
   year: 2005,
-  color: 'white',
+  color: "white",
 };
+
+const showObjectKeys = (obj) => {
+  if (typeof obj === "object") {
+    return Object.keys(obj);
+  }
+  throw new Error("It's not an object");
+};
+
+console.log(showObjectKeys(audi));
--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,23 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+
+  wasExpensive() {
+    return this.budget > 100000000;
+  }
+}
+
+const avatarMovie = new Movie("Avatar", "James Cameron", 237000000);
+
+console.log(avatarMovie);
+console.log(
+  `${avatarMovie.title} movie was expensive ? =>`,
+  avatarMovie.wasExpensive()
+);
