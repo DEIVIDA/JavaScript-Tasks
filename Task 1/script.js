b9cd3966b498b5895976910aad9f1d54c3f796b2
Deivida Gaizauskaite <57774682+DEIVIDA@users.noreply.github.com>
2022-05-17 20:37:18 +0300

    1-5 Tasks

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,41 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const from = document.querySelector("form");
+const outputContainer = document.getElementById("output");
+
+const formatOutputValue = (name, value) => {
+  return `<dt>${name}:</dt><dd>${value}</dd>`;
+};
+
+if (from && output) {
+  from.addEventListener(
+    "submit",
+    function (evt) {
+      evt.preventDefault();
+
+      const formData = new FormData(evt.target);
+      const kg = formData.get("search");
+
+      if (kg.length === 0 || isNaN(parseFloat(kg))) {
+        alert("Įveskite kilogramus");
+      } else {
+        const lb = kg * 2.2046;
+        const g = kg / 0.001;
+        const oz = kg * 35.274;
+
+        let output = "<dl>";
+
+        output += formatOutputValue("Svarai (lb)", lb.toFixed(2));
+        output += formatOutputValue("Gramai (g)", g.toFixed(2));
+        output += formatOutputValue("Uncijos (oz)", oz.toFixed(2));
+
+        output += "</dl>";
+
+        outputContainer.innerHTML = output;
+      }
+    },
+    true
+  );
+}
